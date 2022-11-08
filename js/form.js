a7d096e18d6d48ad49fa06133787883e099a4e7f
LegioAro <58488934+LegioAro@users.noreply.github.com>
2022-11-08 07:56:17 +0200

    Add files via upload

--- js/form.js
+++ js/form.js
@@ -5,30 +5,16 @@ $(
 
       // Ссылка, которую получили на этапе публикации приложения
       let appLink =
-        'https://script.google.com/macros/s/AKfycbxSHw_jQwWMfap8GaIxa2r8Uvvq-yBf1LVlWfs/exec';
+        'https://script.google.com/macros/s/AKfycbzeITkd35jKrIwwwbHqrGjHTCxGnhHL03nDV1ck/exec';
 
-      //   // Сообщение при успешной отправке данных
-      //   let successRespond =
-      //     'Сообщение успешно отправлено. Посмотрите результат <a target="_blank" href="https://docs.google.com/spreadsheets/d/1XcTivCQL4EZJf1x2kB4UwGj5bAi0sfpx1PwtaKgOWHo/edit?usp=sharing">тут</a>';
+      //body
+      let body = $('body');
 
-      //   // Сообщение при ошибке в отправке данных
-      //   let errorRespond =
-      //     'Не удалось отправить сообщение. Cвяжитесь с администратором сайта по адресу <a href="mailto:smart-landing@ya.ru">smart-landing@ya.ru</a>';
+      //активная форма
+      let formVisible = $('.modal__wrapper');
 
       // Id текущей формы
       let form = $('#' + $(this).attr('id'))[0];
-
-      //   // h2 с ответом формы
-      //   let formRespond = $(this).find('.g-form__title_respond');
-
-      //   // h2 с заголовком формы
-      //   let formTitle = $(this).find('.g-form__title_main');
-
-      //   // Блок прелоадера
-      //   let preloader = $(this).find('.g-form__preloader');
-
-      //   // Кнопка отправки формы
-      //   let submitButton = $(this).find('.g-form__button');
 
       // FormData
       let fd = new FormData(form);
@@ -40,75 +26,31 @@ $(
         processData: false,
         contentType: false,
         beforeSend: function () {
-          //   if (fd.get('honeypot').length) {
-          //     return false;
-          //   } else {
-          //     fd.delete('honeypot');
-          //   }
-          //   // Показываем прелоадер
-          //   preloader.css('opacity', '1');
-          //   // Делаем неактивной кнопку отправки
-          //   submitButton.prop('disabled', true);
+          if (fd.get('honeypot').length) {
+            return false;
+          } else {
+            fd.delete('honeypot');
+            fd.delete('Согласие');
+          }
           // валидация других полей.
         },
       }).done(function (res, textStatus, jqXHR) {
         if (jqXHR.readyState === 4 && jqXHR.status === 200) {
-          // Прячем заголовок формы
-          //   formTitle.css({
-          //     display: 'none',
-          //   });
-
-          // // Прячем прелоадер
-          // preloader.css('opacity', '0');
-
-          // // Выводим ответ формы.
-          // formRespond.html(successRespond).css('color', '#37b599');
-
-          // // Возвращаем активность кнопке отправки
-          // submitButton.prop('disabled', false);
-
           // Очищаем поля формы
           form.reset();
+          body.removeClass('lock');
+          formVisible.removeClass('active');
+
+          document.querySelector('.modal__checkbox').checked = false;
+          document.querySelector('.modal__form-btn').disabled = true;
+
+          var link = document.createElement('a');
+          link.href = 'https://t.me/mnmaker_bot';
+          link.setAttribute('target', '_blank');
+          link.click();
+          link.remove();
         }
-        //    else {
-        //     // formTitle.css({
-        //     //   display: 'none',
-        //     // });
-        //     // formRespond.html(errorRespond).css('color', '#c64b4b');
-        //     // preloader.css('opacity', '0');
-        //     // setTimeout(() => {
-        //     //   formRespond.css({
-        //     //     display: 'none',
-        //     //   });
-        //     //   formTitle.css({
-        //     //     display: 'block',
-        //     //   });
-        //     //   submitButton.prop('disabled', false);
-        //     // }, 5000);
-        //     // console.log('Гугл не ответил статусом 200');
-        //   }
       });
-      // .fail(function (res, textStatus, jqXHR) {
-      //   //   formTitle.css({
-      //   //     display: 'none',
-      //   //   });
-      //   //   preloader.css('opacity', '0');
-      //   //   formRespond
-      //   //     .html(
-      //   //       'Не удалось отправить сообщение. Cвяжитесь с администратором сайта другим способом',
-      //   //     )
-      //   //     .css('color', '#c64b4b');
-      //   //   setTimeout(() => {
-      //   //     formRespond.css({
-      //   //       display: 'none',
-      //   //     });
-      //   //     formTitle.css({
-      //   //       display: 'block',
-      //   //     });
-      //   //     submitButton.prop('disabled', false);
-      //   //   }, 5000);
-      //   //   console.log('Не удалось выполнить запрос по указанному в скрипте пути');
-      // });
     });
   })(jQuery),
 );
--- js/main.js
+++ js/main.js
@@ -32,9 +32,21 @@ function isModalClose() {
   }
 }
 
+function isModalAgree() {
+  const modalCheckboxAgree = document.querySelector('.modal__checkbox');
+  const modalCheckboxAgreeChecked = modalCheckboxAgree.checked;
+  const modalBtn = document.querySelector('.modal__form-btn');
+
+  modalBtn.disabled = !modalCheckboxAgreeChecked;
+
+  modalCheckboxAgree.addEventListener('change', isModalAgree);
+}
+
+isModalAgree();
+
 function isTimer() {
   let dateNow = new Date();
-  let minutesDeadline = 80;
+  let minutesDeadline = 7;
   dateNow.setMinutes(dateNow.getMinutes() + minutesDeadline);
 
   function countdownTimer() {
@@ -43,17 +55,17 @@ function isTimer() {
       clearInterval(timerId);
     }
 
-    const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
+    // const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
     const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
     const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
     const milSeconds = diff > 0 ? Math.floor(diff) % 100 : 0;
 
-    let hoursString = hours < 10 ? '0' + hours : String(hours);
+    // let hoursString = hours < 10 ? '0' + hours : String(hours);
     let minutesString = minutes < 10 ? '0' + minutes : String(minutes);
     let secondsString = seconds < 10 ? '0' + seconds : String(seconds);
     let milsecString = milSeconds < 10 ? '0' + milSeconds : String(milSeconds);
 
-    let hoursArr = hoursString.split('');
+    // let hoursArr = hoursString.split('');
     let minutesArr = minutesString.split('');
     let secondsArr = secondsString.split('');
     let milsecArr = milsecString.split('');
@@ -67,18 +79,18 @@ function isTimer() {
     for (let i = 0; i < timerSecondsItems.length; i++) {
       timerSecondsItems[i].innerHTML = secondsArr[i];
     }
-    for (let i = 0; i < timerHoursItems.length; i++) {
-      timerHoursItems[i].innerHTML = hoursArr[i];
-    }
+    // for (let i = 0; i < timerHoursItems.length; i++) {
+    //   timerHoursItems[i].innerHTML = hoursArr[i];
+    // }
   }
   let timer = document.querySelector('.timer');
 
-  let timerHours = timer.querySelector('[data-timer-hours]');
+  // let timerHours = timer.querySelector('[data-timer-hours]');
   let timerMinuts = timer.querySelector('[data-timer-minuts]');
   let timerSeconds = timer.querySelector('[data-timer-seconds]');
   let timerMilSec = timer.querySelector('[data-timer-milsec]');
 
-  let timerHoursItems = timerHours.querySelectorAll('.timer__item-num');
+  // let timerHoursItems = timerHours.querySelectorAll('.timer__item-num');
   let timerMinutesItems = timerMinuts.querySelectorAll('.timer__item-num');
   let timerSecondsItems = timerSeconds.querySelectorAll('.timer__item-num');
   let timerMilSecItems = timerMilSec.querySelectorAll('.timer__item-num');
